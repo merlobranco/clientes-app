fb1ecd660847e35a85ff0ed3280c28e7866afdda
Brais Cidras <braisomary@gmail.com>
2021-01-18 19:55:59 +0200

    Implementing Observable in our service class ClienteService

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] { return CLIENTES; }
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
+  }
 }
