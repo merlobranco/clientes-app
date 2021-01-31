3bffcf6fb589761c4379892dcedf82e9e0c0a62d
Brais Cidras <braisomary@gmail.com>
2021-01-31 22:10:07 +0200

    Creating Cliente form component and view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
 {path: '', redirectTo: '/clientes', pathMatch: 'full'},
@@ -22,11 +24,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     [RouterModule.forRoot(routes)]
   ],
   providers: [ClienteService],
---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent implements OnInit {
+
+  titulo = 'Crear Cliente';
+  cliente: Cliente = new Cliente();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public create(): void {
+    console.log('clicked!');
+    console.log(this.cliente);
+  }
+
+}
