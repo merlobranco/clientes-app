27f744fc8ce7a847e99ca78a5dc34ca43dc2b569
Brais Cidras <braisomary@gmail.com>
2021-02-03 19:29:39 +0200

    Error Handling. Angular Frontend show method

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { Observable } from 'rxjs';
+import { Observable, throwError} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -9,7 +12,7 @@ export class ClienteService {
 
    private httpHeaders = new HttpHeaders({'Content-type': 'application/json'});
 
-   constructor(private http: HttpClient) { }
+   constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlEndpoint);
@@ -20,7 +23,14 @@ export class ClienteService {
   }
 
   getCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        console.log(e.error.mensaje);
+        Swal.fire('Error al consultar cliente', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
   }
 
   update(cliente: Cliente): Observable<Cliente> {
